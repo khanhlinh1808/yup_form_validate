9ebf51839debb87bfff377cc7487a2c8f17283e7
Linh Do <khanhlinhdothi@gmail.com>
2022-05-04 16:30:22 +0700

    changing in app.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,59 @@
-import * as yup from "yup";
-import update from "immutability-helper";
-import { useState, useCallback, memo } from "react";
+import * as yup from 'yup'
+import update from 'immutability-helper'
+import { useState, useCallback, memo } from 'react'
 
-import { Form } from "./Form/Form";
+import { Form } from './Form/Form'
 
 const formSchema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(8).required(),
-});
+})
 
 export const App = memo(() => {
   const [values, setValues] = useState({
-    email: "",
-    password: "",
-  });
+    email: '',
+    password: '',
+  })
   const [errors, setErrors] = useState({
     email: false,
     password: false,
-  });
+  })
   const onFieldChange = useCallback((fieldName, value) => {
     setValues((prevValues) =>
       update(prevValues, {
         [fieldName]: {
           $set: value,
         },
-      })
-    );
-  }, []);
-  console.log("re-render");
+      }),
+    )
+  }, [])
   const onSubmit = useCallback(
     async (e) => {
-      e.preventDefault();
+      e.preventDefault()
 
       const isFormValid = await formSchema.isValid(values, {
         abortEarly: false,
-      });
+      })
       if (isFormValid) {
-        alert("Form validation succeeded");
+        alert('Form validation succeeded')
       } else {
         formSchema.validate(values, { abortEarly: false }).catch((err) => {
           const errors = err.inner.reduce((acc, error) => {
             return {
               ...acc,
               [error.path]: true,
-            };
-          }, {});
+            }
+          }, {})
           setErrors((prevErrors) =>
             update(prevErrors, {
               $set: errors,
-            })
-          );
-        });
+            }),
+          )
+        })
       }
     },
-    [values]
-  );
+    [values],
+  )
 
   return (
     <Form
@@ -63,5 +62,5 @@ export const App = memo(() => {
       onFieldChange={onFieldChange}
       onSubmit={onSubmit}
     />
-  );
-});
+  )
+})
